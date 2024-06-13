1dd00e0e4d3f909127322867066298724edde21f
bbrokyu <chg1028@snu.ac.kr>
2024-06-13 20:01:53 +0000

    최형규

--- main.cpp
+++ main.cpp
@@ -7,13 +7,27 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
+    vec.resize(M);
+    int idx = 0;
+    while (idx >= 0) {
+        if (idx == M) {
+            for (int i = 0; i < M; ++i) {
+                cout << vec[i] << " ";
+            }
+            cout << endl;
+        }
 
+        if (idx < 0) break;
 
+        vec[idx]++;
 
-
-
-
-
+        if (vec[idx] > N) {
+            vec[idx] = 0;
+            idx--;
+        } else if (idx == 0 || vec[idx] > vec[idx - 1]) {
+            idx++;
+        }
+    }
 }
 
 int main() {
